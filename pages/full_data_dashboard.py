9b74ed27872c5238c092fc8e401dc967dbcf2413
John Feng <johnfengphd@gmail.com>
2024-06-28 11:55:21 -0700

    Refactor parse_uploaded_file function to include modules_to_skip parameter - use relative_x_positions - rearranged layout

--- pages/full_data_dashboard.py
+++ pages/full_data_dashboard.py
@@ -58,16 +58,18 @@ else:
 
 
 @st.cache_data
-def parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth, include_background=False):
+def parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth, modules_to_skip=0):
     st.write(uploaded_file.name)
     EM = ExtractModuleStreamlit(uploaded_file)
 
     extracted_df_list = EM.extract_all_modules2df()
-    if not include_background:
-        extracted_df_list = extracted_df_list[1:]
-        N_MODULES = EM.number_of_modules - 1
-    else:
-        N_MODULES = EM.number_of_modules
+    extracted_df_list = extracted_df_list[modules_to_skip:]
+    N_MODULES = EM.number_of_modules - modules_to_skip
+    # if not include_background:
+    #     extracted_df_list = extracted_df_list[1:]
+    #     N_MODULES = EM.number_of_modules - 1
+    # else:
+    #     N_MODULES = EM.number_of_modules
 
     TD = TransformDf()
     df_transformed_list = TD.transform_all_df(extracted_df_list)
@@ -75,8 +77,12 @@ def parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth, include_b
         peak_halfwidth = peak_halfwidth_input
         df_transformed_list = TD.add_peak_counts_all(bin_peak_input, peak_halfwidth)
 
-    x_positions = EM.extract_metadata_list(EM.csv_file, "stage_x_mm:")
-    y_positions = EM.extract_metadata_list(EM.csv_file, "stage_y_mm:")
+    # x_positions = EM.extract_metadata_list(EM.csv_file, "stage_x_mm:")
+    # y_positions = EM.extract_metadata_list(EM.csv_file, "stage_y_mm:")
+    x_positions = EM.extract_metadata_list(EM.csv_file, "stage_x_px:")
+    y_positions = EM.extract_metadata_list(EM.csv_file, "stage_y_px:")
+    x_positions = [float(x) for x in x_positions] # convert list of str to float
+    y_positions = [float(y) for y in y_positions] # convert list of str to float
     heights = EM.extract_metadata_list(EM.csv_file, "height:")
     # st.write(x_positions, y_positions, heights)
 
@@ -117,7 +123,7 @@ if uploaded_file is not None:
         y_positions,
         heights,
         df_transformed_list,
-    ) = parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth_input)
+    ) = parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth_input, modules_to_skip=0)
 
     with st.expander("Data Info", expanded=False):
         st.write(f"Number of modules: {N_MODULES}")
@@ -126,28 +132,26 @@ if uploaded_file is not None:
         st.write(f"Height: {heights}")
 
     # create a slider to select the module
-    module_index = st.sidebar.slider("Select a module:", 0, N_MODULES - 1, 0)
-
-    with st.expander("Average Spectrum", expanded=False):
-        spectrum_avg_fig = create_spectrum_average(
-            df_transformed_list[module_index],
-            bin_peak=bin_peak_input,
-            peak_halfwidth=peak_halfwidth_input,
-        )
-        st.plotly_chart(spectrum_avg_fig)
+    # module_index = st.sidebar.slider("Select a module:", 0, N_MODULES - 1, 0)
 
     with st.expander("HEATMAP and PIXEL SPECTRUM", expanded=True):
-        
+        relative_x_positions = [round(x-x_positions[1], 2) for x in x_positions]
+        relative_y_positions = [round(y-y_positions[1], 2) for y in y_positions]
+
         plot_columns = st.columns([1, 1])
         
         with plot_columns[0]:
+            # create a slider to select the module
+            module_index = st.slider("Select a module:", 0, N_MODULES - 1, 0)
             heatmap_fig = create_pixelized_heatmap(
                 df_transformed_list[module_index],
                 count_type=count_type,
                 normalization=normalize_check,
                 color_scale=color_scale,
             )
+            heatmap_fig.update_layout(title=f"X-stage: {relative_x_positions[module_index]} px,  Y-stage: {relative_y_positions[module_index]} px")
             st.plotly_chart(heatmap_fig)
+            
             
         with plot_columns[1]:
             
@@ -178,7 +182,7 @@ if uploaded_file is not None:
 
             st.session_state.pixel_indices = pixel_selections
             range_slider = st.slider(
-                "Select a range of bins:",
+                "range slider (bins)",
                 min_value=0,
                 max_value=199 ,
                 value=(80,120),
@@ -192,44 +196,58 @@ if uploaded_file is not None:
                 peak_halfwidth=peak_halfwidth_input,
                 x_range=range_slider,
             )
-            pixel_spectrum_figure.update_layout(title=f"Select Pixel Spectrum")
+            pixel_spectrum_figure.update_layout(title="Select Pixel Spectrum")
             
             st.plotly_chart(pixel_spectrum_figure)
+
+    with st.expander("Average Spectrum", expanded=False):
+        spectrum_avg_fig = create_spectrum_average(
+            df_transformed_list[module_index],
+            bin_peak=bin_peak_input,
+            peak_halfwidth=peak_halfwidth_input,
+        )
+        st.plotly_chart(spectrum_avg_fig)
 
     with st.expander("Sweep analysis", expanded=True):
         # st.write(f"Stage X position: {x_positions}")
         # st.write(f"Stage Y position: {y_positions}")
         # st.write(f"Height: {heights}")
-        x_positions = [float(x) for x in x_positions]
-        x_positions = [round(x-x_positions[0], 2) for x in x_positions]
+        relative_x_positions = [round(x-x_positions[1], 2) for x in x_positions]
         data_range = st.slider(
-            "Select a module:",
+            "Modules to include:",
             min_value=0,
             max_value=N_MODULES ,
-            value=(0, N_MODULES ),
+            value=(1, N_MODULES ),
             step=1,
         )
-        columns = st.columns([1, 1])
+        columns = st.columns([1, 1], gap="large")
         with columns[0]:
-            range_slider_x = st.slider(
-                "Select a range of bins:",
-                min_value=0,
-                max_value=199 ,
-                value=(80,120),
-                step=1,
-                key="range_slider_x",)
-            range_slider_y = st.slider(
-                "Select a range of counts:",
-                min_value=0,
-                max_value=200,
-                value=(0,120),
-                step=1,
-                key="range_slider_y",)
+            sub_columns = st.columns([3, 1])
+            with sub_columns[0]:
+                range_slider_x = st.slider(
+                    "X-range slider (bins)",
+                    min_value=0,
+                    max_value=199 ,
+                    value=(80,120),
+                    step=1,
+                    key="range_slider_x",)
+            with sub_columns[1]:
+                # range_slider_y = st.slider(
+                #     "Y-range slider (Counts)",
+                #     min_value=0,
+                #     max_value=500,
+                #     value=(0,120),
+                #     step=1,
+                #     key="range_slider_y",)
+                y_max = st.number_input("Max Counts", value=500, step=1)
             
             # x_choice = pixel_selectbox("X", "Pixel", 101)
         with columns[1]:
-            x_choice = pixel_selectbox("X", "Pixel", 101)
-            y_choice = pixel_selectbox("Y", "Pixel", 102)
+            sub_columns = st.columns([1, 1])
+            with sub_columns[0]:
+                x_choice = pixel_selectbox("X", "Pixel", 101)
+            with sub_columns[1]:
+                y_choice = pixel_selectbox("Y", "Pixel", 102)
             
         spectrum_sweep = create_spectrum_pixel_sweep(
             df_transformed_list,
@@ -238,8 +256,9 @@ if uploaded_file is not None:
             min_data_range=data_range[0],
             max_data_range=data_range[1],
             x_range=range_slider_x,
-            y_range=range_slider_y,
-            stage_x_mm=x_positions,
+            # y_range=range_slider_y,
+            y_range=[0, y_max],
+            stage_x_mm=relative_x_positions,
         )
         with columns[0]:
             st.plotly_chart(spectrum_sweep)
@@ -253,7 +272,8 @@ if uploaded_file is not None:
             max_data_range=data_range[1],
             x_range=[75, 125],
             y_range=[0, 120],
-            stage_x_mm=x_positions,
+            stage_x_mm=relative_x_positions,
         )
+        count_sweep.update_layout(xaxis_title="Relative Stage X (mm)")
         with columns[1]:
             st.plotly_chart(count_sweep)
