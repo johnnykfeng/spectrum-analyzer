c3c9bc3959a12efb45b0d885496ec927d390e73b
John Feng <johnfengphd@gmail.com>
2024-06-28 21:48:25 -0700

    implement app defaults based on source - source radio button added - allow switching between x and y stage positions

--- pages/full_data_dashboard.py
+++ pages/full_data_dashboard.py
@@ -7,11 +7,15 @@ from plotting_modules import (
     create_spectrum_pixel,
     create_pixelized_heatmap,
     create_spectrum_pixel_sweep,
-    create_count_sweep
+    create_count_sweep,
 )
 
-st.set_page_config("Full Data Dashboard", page_icon="📊",
-                   layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    "Full Data Dashboard",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 
 @st.cache_data
@@ -39,11 +43,40 @@ count_type = st.sidebar.radio(
 
 normalize_check = st.sidebar.checkbox("Normalize heatmap")
 
-bin_peak_input = st.sidebar.number_input("Default bin peak", step=1, value=95)
+source = st.sidebar.radio("Radiation Source", ("Am241", "Co57", "Cs137"))
+app_defaults = {
+    "Am241": {
+        "bin_peak": 95,
+        "peak_halfwidth": 22,
+        "max_bin": 199,
+        "bin_range": (80, 125),
+        "max_counts": 500,
+    },
+    "Co57": {
+        "bin_peak": 243,
+        "peak_halfwidth": 22,
+        "max_bin": 300,
+        "bin_range": (100, 300),
+        "max_counts": 500,
+    },
+    "Cs137": {
+        "bin_peak": 1800,
+        "peak_halfwidth": 22,
+        "max_bin": 1999,
+        "bin_range": (100, 1900),
+        "max_counts": 500,
+    },
+}
+
+bin_peak_input = st.sidebar.number_input(
+    "Default bin peak", step=1, value=app_defaults[source]["bin_peak"]
+)
 
 peak_halfwidth_input = st.sidebar.number_input(
-    "Default peak halfwidth", value=50, step=1
+    "Default peak halfwidth", value=app_defaults[source]["peak_halfwidth"], step=1
 )
+
+# st.write(f"Default bin peak: {app_defaults[source]['bin_range']}")
 
 st.warning("Only File uploader is working for now")
 upload_type = st.radio("File upload type", ("File uploader", "Directory input"))
@@ -58,18 +91,15 @@ else:
 
 
 @st.cache_data
-def parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth, modules_to_skip=0):
+def parse_uploaded_file(
+    uploaded_file, bin_peak_input, peak_halfwidth, modules_to_skip=0
+):
     st.write(uploaded_file.name)
     EM = ExtractModuleStreamlit(uploaded_file)
 
     extracted_df_list = EM.extract_all_modules2df()
     extracted_df_list = extracted_df_list[modules_to_skip:]
     N_MODULES = EM.number_of_modules - modules_to_skip
-    # if not include_background:
-    #     extracted_df_list = extracted_df_list[1:]
-    #     N_MODULES = EM.number_of_modules - 1
-    # else:
-    #     N_MODULES = EM.number_of_modules
 
     TD = TransformDf()
     df_transformed_list = TD.transform_all_df(extracted_df_list)
@@ -81,8 +111,8 @@ def parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth, modules_t
     # y_positions = EM.extract_metadata_list(EM.csv_file, "stage_y_mm:")
     x_positions = EM.extract_metadata_list(EM.csv_file, "stage_x_px:")
     y_positions = EM.extract_metadata_list(EM.csv_file, "stage_y_px:")
-    x_positions = [float(x) for x in x_positions] # convert list of str to float
-    y_positions = [float(y) for y in y_positions] # convert list of str to float
+    x_positions = [float(x) for x in x_positions]  # convert list of str to float
+    y_positions = [float(y) for y in y_positions]  # convert list of str to float
     heights = EM.extract_metadata_list(EM.csv_file, "height:")
     # st.write(x_positions, y_positions, heights)
 
@@ -115,7 +145,7 @@ if "num_pixels" not in st.session_state:
     st.session_state.num_pixels = 2
 
 if "counts_max_pixel" not in st.session_state:
-    st.session_state.counts_max_pixel = 15
+    st.session_state.counts_max_pixel = app_defaults[source]["max_counts"]
 
 if uploaded_file is not None:
     (
@@ -126,7 +156,9 @@ if uploaded_file is not None:
         y_positions,
         heights,
         df_transformed_list,
-    ) = parse_uploaded_file(uploaded_file, bin_peak_input, peak_halfwidth_input, modules_to_skip=0)
+    ) = parse_uploaded_file(
+        uploaded_file, bin_peak_input, peak_halfwidth_input, modules_to_skip=0
+    )
 
     with st.expander("Data Info", expanded=False):
         st.write(f"Number of modules: {N_MODULES}")
@@ -138,12 +170,12 @@ if uploaded_file is not None:
     # module_index = st.sidebar.slider("Select a module:", 0, N_MODULES - 1, 0)
 
     with st.expander("HEATMAP and PIXEL SPECTRUM", expanded=True):
-        relative_x_positions = [round(x-x_positions[1], 2) for x in x_positions]
-        relative_y_positions = [round(y-y_positions[1], 2) for y in y_positions]
+        relative_x_positions = [round(x - x_positions[1], 2) for x in x_positions]
+        relative_y_positions = [round(y - y_positions[1], 2) for y in y_positions]
 
-        plot_columns = st.columns([1, 1])
-        
-        with plot_columns[0]:
+        heatmap_panel, pixel_spectrum_panel = st.columns([1, 1])
+
+        with heatmap_panel:
             # create a slider to select the module
             module_index = st.slider("Select a module:", 0, N_MODULES - 1, 0)
             heatmap_fig = create_pixelized_heatmap(
@@ -152,12 +184,12 @@ if uploaded_file is not None:
                 normalization=normalize_check,
                 color_scale=color_scale,
             )
-            heatmap_fig.update_layout(title=f"X-stage: {relative_x_positions[module_index]} px,  Y-stage: {relative_y_positions[module_index]} px")
+            heatmap_fig.update_layout(
+                title=f"X-stage: {relative_x_positions[module_index]} px,  Y-stage: {relative_y_positions[module_index]} px"
+            )
             st.plotly_chart(heatmap_fig)
-            
-            
-        with plot_columns[1]:
-            
+
+        with pixel_spectrum_panel:
             num_pixels = st.number_input(
                 "Number of pixels to display",
                 min_value=1,
@@ -176,31 +208,36 @@ if uploaded_file is not None:
                     ):
                         # last_x_index = st.session_state.pixel_indices[c][0]
                         # last_y_index = st.session_state.pixel_indices[c][1]
-                        x_index = pixel_selectbox("X", c, module_index, st.session_state.pixel_indices[c][0])
-                        y_index = pixel_selectbox("Y", c, module_index, st.session_state.pixel_indices[c][1])
+                        x_index = pixel_selectbox(
+                            "X", c, module_index, st.session_state.pixel_indices[c][0]
+                        )
+                        y_index = pixel_selectbox(
+                            "Y", c, module_index, st.session_state.pixel_indices[c][1]
+                        )
                     else:
                         x_index = pixel_selectbox("X", c, module_index)
                         y_index = pixel_selectbox("Y", c, module_index)
                     pixel_selections.append((x_index, y_index))
 
             st.session_state.pixel_indices = pixel_selections
-            
 
-            
             sub_columns = st.columns([4, 1], gap="large")
             with sub_columns[0]:
                 range_slider = st.slider(
                     "range slider (bins)",
                     min_value=0,
-                    max_value=199 ,
-                    value=(80,120),
-                    step=1,)
+                    max_value=app_defaults[source]["max_bin"],
+                    value=app_defaults[source]["bin_range"],
+                    step=1,
+                )
             with sub_columns[1]:
                 # last_counts_max_pixel = st.session_state.counts_max_pixel
-                st.session_state.counts_max_pixel = st.number_input("Max Counts", 
-                                                   value=st.session_state.counts_max_pixel, step=1,
-                                                    key=f"y_max_{module_index}")
-                    
+                st.session_state.counts_max_pixel = st.number_input(
+                    "Max Counts",
+                    value=st.session_state.counts_max_pixel,
+                    step=1,
+                    key=f"y_max_pixel_select",
+                )
 
             pixel_spectrum_figure = create_spectrum_pixel(
                 df_transformed_list[module_index],
@@ -211,7 +248,7 @@ if uploaded_file is not None:
                 y_range=[0, st.session_state.counts_max_pixel],
             )
             pixel_spectrum_figure.update_layout(title="Select Pixel Spectrum")
-            
+
             st.plotly_chart(pixel_spectrum_figure)
 
     with st.expander("Average Spectrum", expanded=False):
@@ -219,67 +256,81 @@ if uploaded_file is not None:
             df_transformed_list[module_index],
             bin_peak=bin_peak_input,
             peak_halfwidth=peak_halfwidth_input,
+            x_range=[0, app_defaults[source]["max_bin"]],
+            y_range=[0, app_defaults[source]["max_counts"]],
         )
         st.plotly_chart(spectrum_avg_fig)
 
-    with st.expander("Sweep analysis", expanded=True):
-        # st.write(f"Stage X position: {x_positions}")
-        # st.write(f"Stage Y position: {y_positions}")
-        # st.write(f"Height: {heights}")
-        relative_x_positions = [round(x-x_positions[1], 2) for x in x_positions]
+    with st.expander("SWEEP ANALYSIS", expanded=True):
+        relative_x_positions = [round(x - x_positions[1], 2) for x in x_positions]
+        relative_y_positions = [round(y - y_positions[1], 2) for y in y_positions]
+        axes_choice = {"X-abs": x_positions, 
+                       "Y-abs": y_positions, 
+                       "X-relative": relative_x_positions, 
+                       "Y-relative": relative_y_positions}
+        
         data_range = st.slider(
             "Modules to include:",
             min_value=0,
-            max_value=N_MODULES ,
-            value=(1, N_MODULES ),
+            max_value=N_MODULES,
+            value=(1, N_MODULES),
             step=1,
         )
-        columns = st.columns([1, 1], gap="large")
-        with columns[0]:
-            sub_columns = st.columns([3, 1])
+
+        left_panel, right_panel = st.columns([1, 1], gap="large")
+        with left_panel:
+            sub_columns = st.columns([3, 1], gap="large")
             with sub_columns[0]:
                 range_slider_x = st.slider(
                     "X-range slider (bins)",
                     min_value=0,
-                    max_value=199 ,
-                    value=(80,120),
+                    max_value=app_defaults[source]["max_bin"],
+                    value=app_defaults[source]["bin_range"],
                     step=1,
-                    key="range_slider_x",)
+                    key="range_slider_x",
+                )
             with sub_columns[1]:
-                y_max = st.number_input("Max Counts", value=500, step=1)
+                y_max = st.number_input(
+                    "Max Counts",
+                    value=app_defaults[source]["max_counts"],
+                    step=1,
+                    key="y_max_sweep",
+                )
+
+        with right_panel:
+            axes_radio = st.radio("sweep axis", ("X-abs", "Y-abs", "X-relative", "Y-relative"),
+                                  horizontal=True, key="axes_radio")
             
-            # x_choice = pixel_selectbox("X", "Pixel", 101)
-        with columns[1]:
             sub_columns = st.columns([1, 1])
             with sub_columns[0]:
                 x_choice = pixel_selectbox("X", "Pixel", 101)
             with sub_columns[1]:
                 y_choice = pixel_selectbox("Y", "Pixel", 102)
-            
-        spectrum_sweep = create_spectrum_pixel_sweep(
-            df_transformed_list,
-            x_index=x_choice,
-            y_index=y_choice,
-            min_data_range=data_range[0],
-            max_data_range=data_range[1],
-            x_range=range_slider_x,
-            y_range=[0, y_max],
-            stage_x_mm=relative_x_positions,
-        )
-        with columns[0]:
+
+        with left_panel:
+            spectrum_sweep = create_spectrum_pixel_sweep(
+                df_transformed_list,
+                x_index=x_choice,
+                y_index=y_choice,
+                min_data_range=data_range[0],
+                max_data_range=data_range[1],
+                x_range=range_slider_x,
+                y_range=[0, y_max],
+                stage_x_mm = axes_choice[axes_radio],
+            )
             st.plotly_chart(spectrum_sweep)
-        
-        count_sweep = create_count_sweep(
-            df_transformed_list,
-            count_type=count_type,
-            x_index=x_choice,
-            y_index=y_choice,
-            min_data_range=data_range[0],
-            max_data_range=data_range[1],
-            x_range=[75, 125],
-            y_range=[0, 120],
-            stage_x_mm=relative_x_positions,
-        )
-        count_sweep.update_layout(xaxis_title="Relative Stage X (mm)")
-        with columns[1]:
+
+        with right_panel:
+            count_sweep = create_count_sweep(
+                df_transformed_list,
+                count_type=count_type,
+                x_index=x_choice,
+                y_index=y_choice,
+                min_data_range=data_range[0],
+                max_data_range=data_range[1],
+                x_range=[75, 125],
+                y_range=[0, 120],
+                stage_x_mm = axes_choice[axes_radio],
+            )
+            count_sweep.update_layout(xaxis_title=f"{axes_radio} Stage Position (mm)")
             st.plotly_chart(count_sweep)
