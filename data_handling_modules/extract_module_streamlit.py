dce47441b1d7cb70b81762a4b999897a359aef22
John Feng <johnfengphd@gmail.com>
2024-09-01 16:01:43 -0700

    delete useless attributes

--- data_handling_modules/extract_module_streamlit.py
+++ data_handling_modules/extract_module_streamlit.py
@@ -12,15 +12,9 @@ class ExtractModuleStreamlit:
         self.csv_file = csv_file  # file path of the csv file from H3D software
         self.target_string = "H3D_Pixel"  # string to search for in the csv file
         self.number_of_pixels = 121  # determines number of rows to extract from csv file
-        self.n_pixels_x = 11  # number of pixels in x direction
-        self.n_pixels_y = 11  # number of pixels in y direction
         self.line_numbers = []
-        self.start_line = None  # output of find_start_end_lines
-        self.end_line = None  # output of find_start_end_lines
-        self.extra_lines = 0
         self.dataframe = None  # output of extract_module2df
         self.df_list = []  # output of extract_all_modules2df
-        self.N_DF = 0  # number of DataFrames in the list
 
     @staticmethod
     def find_line_number(csv_file, target_string):
