f71298de618f26b835a3683fe874bd08d1c136c0
John Feng <johnfengphd@gmail.com>
2025-04-07 09:23:58 -0700

    remove second page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 pages = [
     st.Page("st_pages/full_data_dashboard.py", title=" 📈 Full Data Dashboard",),
-    st.Page("st_pages/heatmap_spectrum.py", title=" 📊 Heatmap and Spectrum Analysis"),
+    # st.Page("st_pages/heatmap_spectrum.py", title=" 📊 Heatmap and Spectrum Analysis"),
     st.Page("st_pages/hole_projection.py", title=" 🔍 Hole Projection"),
     st.Page("st_pages/intensity_projection.py", title=" 💡 Intensity Projection"),
 ]
